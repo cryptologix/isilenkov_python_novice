8efba310b717c915cf7460ce537a7665aa1d9938
Ilia Silenkov <cryptologix@protonmail.com>
2017-07-07 10:36:34 +0300

    Изменения передачи параметров
    Теперь параметры для создания группы передаются через созданный group.py объект

--- add_group.py
+++ add_group.py
@@ -23,7 +23,7 @@ class add_group(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.create_new_group(wd,"Test","Add new group","with parameters")
+        self.create_new_group(wd,Group("Test","Add new group","with parameters"))
         self.return_to_group_page(wd)
         self.logout(wd)
 
@@ -35,8 +35,8 @@ class add_group(unittest.TestCase):
 
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd, "", "")
-        self.create_new_group(wd,"","","")
+        self.login(wd, username="", password="")
+        self.create_new_group(wd,Group(name="",header="",footer=""))
         self.return_to_group_page(wd)
         self.logout(wd)
 
@@ -54,13 +54,13 @@ class add_group(unittest.TestCase):
         wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(Group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(Group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(Group.footer)
         wd.find_element_by_name("submit").click()
 
     def login(self, wd, username, password):
