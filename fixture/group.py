140a3ed4d33c86e0b81ae65ae852763f238f7829
Ilia Silenkov <cryptologix@protonmail.com>
2017-07-08 10:43:26 +0300

    Добавлен тест на удаление группы

--- fixture/group.py
+++ fixture/group.py
@@ -22,3 +22,10 @@ class groupHelper:
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(Group.footer)
         wd.find_element_by_name("submit").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_group_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
+        self.open_group_page()
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,16 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
-
-from fixture.application import Application
 from model.classes import Group
-
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
